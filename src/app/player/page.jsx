1ff0b5705b5c5a2031c65d835d491c0ce4ed97dd
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-02-21 04:53:23 +0100

    player page login logic negated

--- src/app/player/page.jsx
+++ src/app/player/page.jsx
@@ -27,8 +27,8 @@ const Page = () => {
     "https://t3.ftcdn.net/jpg/05/16/27/58/240_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg";
 
   useEffect(() => {
-    if (isLoggedIn) {
-      router.push("/player");
+    if (!isLoggedIn) {
+      router.push("/login");
     }
   }, [isLoggedIn]);
 
